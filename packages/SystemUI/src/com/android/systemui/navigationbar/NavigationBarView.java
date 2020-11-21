ee16f8f15b4d494f4b43ba86e35bac6df51f6949
ezio84 <brabus84@gmail.com>
2020-11-21 21:16:02 +0100

    Advanced navigation "extended/L" gestures [1/2]
    Allow extended/"L" back swipe to trigger actions/app/activities [1/2]
    
    screen off on long swipe is pretty nice :P
    
    Change-Id: I2068cfce12d84daaaf5dce5380157dffe6255081
    
    Allow a wider back swipe to trigger actions (instead of timeout) [1/2]
    
    swipe for more than 3/4 of X axys display size to trigger action
    instead of long swipe timeout
    
    Change-Id: I3afea79e3ece99b3c5e2c1b53c1516e48337576a
    
    Allow a "L" back swipe to trigger more actions [1/2]
    
    even a fucked up "L" is good to trigger the action :D
    
    also don't break normal back swipe if no action is set as extended swipe
    action
    
    Change-Id: I870f06b76bd9bacee262aa711e4af35ba8cb1f07
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Add switch last app to gesture actions [1/2]
    
    also move it to ActionUtils
    
    Change-Id: I3690677bce0cdc85951b38790cf9374346b1e6c2
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    LongSwipeGestures: Vibrate on action if gesture haptic feedback enabled
    
    Change-Id: I4a17649320789497490ea4a2b6681dc51760d3b0
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Allow back swipe on bottom screen if launcher is showing
    
    on the very bottom area of the screen (1/4 of display size)
    so we can trigger our actions everywhere
    
    Change-Id: I0ca2f5981664864d19a2560a77cfea6e95d304ae
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Extended swipe: show a double arrow when almost triggered
    
    abcdu: adapted to our code
    Change-Id: If8905cb231e5ea0e993a590ce7f02669b46b8136
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Advanced nav gestures: improve code and fix glitches
    
    fix  arrows animation glitch after triggering an action
    with the extended swipe
    
    hide the arrows before triggering a screenshot action
    
    code cleanup
    
    Change-Id: I5f01938b6b688acd14225f58ce5a2032f083f92f
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -628,6 +628,7 @@ public class NavigationBarView extends FrameLayout implements
         }
         mImeVisible = visible;
         mRotationButtonController.getRotationButton().setCanShowRotationButton(!visible);
+        mEdgeBackGestureHandler.setImeVisible(visible);
     }
 
     void setDisabledFlags(int disabledFlags, SysUiState sysUiState) {
