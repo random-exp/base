f956f65eb82e7e0d6becc67ab41c5df6e529cf7c
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:35:11 -0700

    SystemUI: Add API for runtime taskbar config
    Change-Id: Ic2616ff974e98e03edbcdbdf677fa40b40b37abb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6860,6 +6860,11 @@ public final class Settings {
         public static final String STATUSBAR_LEFT_PADDING = "statusbar_left_padding";
 
         /**
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * Statusbar right padding
          * @hide
          */
