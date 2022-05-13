6d9b2f4f8c675cbb014b6a7e717332f0b23a1283
IDontCare-05 <IDontCare-05@gmail.com>
2022-05-13 20:27:47 +0000

    Add tunnings for status bar paddings [1/2]
    Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>
    [str: use statusbar paddings as default parameters]
    
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>
    
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    [include top padding customization]
    [explicitly remove tunable]
    [code optimization]
    [port to A14]
    
    Change-Id: Icbe271cfe7fc465c80707d19071d25345a541f00

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6792,6 +6792,24 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * Statusbar left padding
+         * @hide
+         */
+        public static final String STATUSBAR_LEFT_PADDING = "statusbar_left_padding";
+
+        /**
+         * Statusbar right padding
+         * @hide
+         */
+        public static final String STATUSBAR_RIGHT_PADDING = "statusbar_right_padding";
+
+        /**
+         * Statusbar top padding
+         * @hide
+         */
+        public static final String STATUSBAR_TOP_PADDING = "statusbar_top_padding";
+
+        /**
          * These are all public system settings
          *
          * @hide
@@ -6935,6 +6953,9 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LS_MEDIA_ARTWORK_FADE_PERCENT);
             PRIVATE_SETTINGS.add(AMBIENT_TEXT_COLOR);
             PRIVATE_SETTINGS.add(AMBIENT_TEXT_TYPE_COLOR);
+            PRIVATE_SETTINGS.add(STATUSBAR_LEFT_PADDING);
+            PRIVATE_SETTINGS.add(STATUSBAR_RIGHT_PADDING);
+            PRIVATE_SETTINGS.add(STATUSBAR_TOP_PADDING);
         }
 
         /**
