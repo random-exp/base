110c04426d28f81e2da0fee8901f191da47e3363
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-09-05 04:14:52 +0000

    base: support per-app volume [2/3]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Iaa97787d3820b4f6c23fda678ec3ffe711061c4f
    
    Change-Id: I06e6bd5c782e5e858501647f912e07a468b259c6
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6270,6 +6270,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -20402,6 +20408,12 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_MOBILE_DATA =
                 "android.settings.panel.action.MOBILE_DATA";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
