0ae448b7c29d0212042b4b659d6c8f263964cffd
someone5678 <nemui3353@gmail.com>
2023-06-25 12:00:51 +0900

    base: Guard brightness slider haptic by setting [1/2]
    Change-Id: Ic42c037da8d7cc8378f084ed20205fafc52b95e5
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6637,6 +6637,12 @@ public final class Settings {
         public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
 
         /**
+         * Give haptic feedback on brightness slider changes.
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+
+        /**
          * These are all public system settings
          *
          * @hide
