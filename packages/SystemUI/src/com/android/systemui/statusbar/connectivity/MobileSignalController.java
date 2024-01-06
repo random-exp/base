a19a991e5863b18c4f812b06a5958380792c3449
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-06 21:17:09 +0530

    SystemUI: Do not duplicate SPN and PLMN in network name
    Same as the logic in telephony framework, show only the PLMN if it
    already contains the SPN, insensitive of case. This avoids showing
    carrier name in statusbar twice, for example, "JIO - Jio" or
    "Jio True5G - JIO".
    
    Change-Id: Ia9c1022fe314c6f4c1ff7db6e85dba66fc390afc

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -577,7 +577,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
             str.append(plmn);
             strData.append(plmn);
         }
-        if (showSpn && spn != null && !str.toString().equalsIgnoreCase(spn)) {
+        if (showSpn && spn != null && !str.toString().toLowerCase().contains(spn.toLowerCase())) {
             if (str.length() != 0) {
                 str.append(mNetworkNameSeparator);
             }
@@ -588,7 +588,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         } else {
             mCurrentState.networkName = mNetworkNameDefault;
         }
-        if (showSpn && dataSpn != null && !strData.toString().equalsIgnoreCase(dataSpn)) {
+        if (showSpn && dataSpn != null &&
+                !strData.toString().toLowerCase().contains(dataSpn.toLowerCase())) {
             if (strData.length() != 0) {
                 strData.append(mNetworkNameSeparator);
             }
