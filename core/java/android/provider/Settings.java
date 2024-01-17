omnirom/android_frameworks_base_old

SystemUI: Pulse light accent color option
Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2

core/java/android/provider/Settings.java
@@ -5098,6 +5098,16 @@ public final class Settings {
         private static final Validator OMNI_AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+
+        public static final String OMNI_AMBIENT_NOTIFICATION_LIGHT_ACCENT = "ambient_notification_light_accent";
+
+        private static final Validator OMNI_AMBIENT_NOTIFICATION_LIGHT_ACCENT_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
         /**
          * helper setting to contain the current pulse reason so we can check
          * if it has been triggered from a notification or a gesture
@@ -5267,6 +5277,7 @@ public final class Settings {
             OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED,
             OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED,
             OMNI_AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD,
+            OMNI_AMBIENT_NOTIFICATION_LIGHT_ACCENT,
         };
 
         /**
@@ -5645,7 +5656,7 @@ public final class Settings {
             VALIDATORS.put(OMNI_AMBIENT_NOTIFICATION_LIGHT, OMNI_AMBIENT_NOTIFICATION_LIGHT_VALIDATOR);
             VALIDATORS.put(OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED_VALIDATOR);
             VALIDATORS.put(OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED, OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED_VALIDATOR);
-            VALIDATORS.put(OMNI_AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD, OMNI_AMBIENT_NOTIFICATION_LIGHT_HIDE_AOD_VALIDATOR);
+            VALIDATORS.put(OMNI_AMBIENT_NOTIFICATION_LIGHT_ACCENT, OMNI_AMBIENT_NOTIFICATION_LIGHT_ACCENT_VALIDATOR);
         }
 
         /**

random-exp/base

Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

core/java/android/provider/Settings.java
@@ -6378,7 +6378,13 @@ public final class Settings {
          * if it has been triggered from a notification or a gesture
          * @hide
          */
-        public static final String OMNI_PULSE_TRIGGER_REASON = "pulse_trigger_reason";
+        public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
+
+        /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT_ACCENT = "ambient_notification_light_accent";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to