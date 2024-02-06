Ritikk11/android_frameworks_base_ext

[SQUASH]Import PixelPropsUtils
- Squash of 150+ commits..

Co-authored-by: Danny Lin <danny@kdrag0n.dev>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: aswin7469 <aswinas@pixysos.com>
Co-authored-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
Co-authored-by: Dyneteve <dyneteve@hentaios.com>
Co-authored-by: Anay Wadhera <awadhera@berkeley.edu>
Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Co-authored-by: someone5678 <nemui3353@gmail.com>
Co-authored-by: johnmart19 <johnivan19999@gmail.com>
Co-authored-by: AnierinB <anierin@evolution-x.org>
Change-Id: I4185ba819322da128941136fedaf394b04e452a0
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

core/java/android/app/ApplicationPackageManager.java
@@ -137,7 +137,6 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -811,30 +810,20 @@ public class ApplicationPackageManager extends PackageManager {
                 }
             };
 
-    private static final Set<String> googlePackages = new HashSet<>(Arrays.asList(
-            "com.android.vending",
-            "com.google.android.gms",
-            "com.google.android.googlequicksearchbox",
-            "com.google.android.inputmethod.latin",
-            "com.google.android.setupwizard",
-            "com.google.android.settings.intelligence",
-            "com.google.android.apps.miphone.aiai.AiaiApplication",
-            "com.google.android.apps.googleassistant",
-            "com.google.android.as.oss",
-            "com.google.android.as",
-            "com.google.android.apps.recorder"
-    ));
+    private static final String[] pTensorCodenames = {
+            "husky",
+            "shiba",
+            "felix",
+            "tangorpro",
+            "lynx",
+            "cheetah",
+            "panther",
+            "bluejay",
+            "oriole",
+            "raven"
+    };
 
-    private static final Set<String> featuresPixelTensor = new HashSet<>(Arrays.asList(
-            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE"
-    ));
-
-    private static final Set<String> featuresPixel = new HashSet<>(Arrays.asList(
+    private static final String[] featuresPixel = {
             "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
             "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
             "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
@@ -845,6 +834,14 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.feature.LILY_EXPERIENCE",
             "com.google.android.feature.TURBO_PRELOAD",
             "com.google.android.feature.WELLBEING",
+            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2020_EXPERIENCE",
             "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2019_EXPERIENCE",
@@ -864,37 +861,40 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.feature.EXCHANGE_6_2",
             "com.google.android.apps.dialer.call_recording_audio",
             "com.google.android.apps.dialer.SUPPORTED"
-    ));
+    };
 
-    private static final Set<String> featuresNexus = new HashSet<>(Arrays.asList(
+    private static final String[] featuresTensor = {
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE"
+    };
+
+    private static final String[] featuresNexus = {
             "com.google.android.apps.photos.NEXUS_PRELOAD",
             "com.google.android.apps.photos.nexus_preload"
-    ));
+    };
     
-    private static final Set<String> featuresAndroid = new HashSet<>(Arrays.asList(
+    private static final String[] featuresAndroid = {
             "android.software.freeform_window_management"
-    ));
+    };
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
-        String packageName = ActivityThread.currentPackageName();
-        if ("com.google.android.apps.photos".equals(packageName)) {
-            if (featuresPixel.contains(name) || featuresPixelTensor.contains(name)) {
-                return false;
-            }
-            if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true) && featuresNexus.contains(name)) {
-                return true;
-            }
-        } else if (googlePackages.contains(packageName)) {
-            if (featuresPixelTensor.contains(name)) {
-                return false;
-            }
-            if (featuresPixel.contains(name)) {
-                return true;
-            }
-        } else if (featuresPixelTensor.contains(name) || featuresPixel.contains(name) || featuresAndroid.contains(name)) {
-            return true;
+        if (name != null && Arrays.asList(featuresTensor).contains(name) &&
+                !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.superior.device"))) {
+            return false;
         }
+        String packageName = ActivityThread.currentPackageName();
+        if (packageName != null &&
+                packageName.equals("com.google.android.apps.photos")) {
+            if (Arrays.asList(featuresPixel).contains(name)) return false;
+            if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
+                if (Arrays.asList(featuresNexus).contains(name)) return true;
+            }
+        }
+        if (Arrays.asList(featuresPixel).contains(name)) return true;
+        if (Arrays.asList(featuresAndroid).contains(name)) return true;
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
     }
 

core/java/android/app/Instrumentation.java
@@ -59,8 +59,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.PropImitationHooks;
-import com.android.internal.util.superior.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -70,6 +68,8 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.superior.MeizuPropsUtils;
+import com.android.internal.util.superior.PixelPropsUtils;
+import com.android.internal.util.superior.GamesPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1285,12 +1285,12 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PropImitationHooks.setProps(app);
         MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(app);
         GamesPropsUtils.setProps(app);
         return app;
     }
-    
+
     /**
      * Perform instantiation of the process's {@link Application} object.  The
      * default implementation provides the normal system behavior.
@@ -1305,8 +1305,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PropImitationHooks.setProps(app);
         MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(app);
         GamesPropsUtils.setProps(app);
         return app;
     }

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,7 +45,6 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.util.PropImitationHooks;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -88,6 +87,8 @@ import java.util.Locale;
 import java.util.NoSuchElementException;
 
 import javax.crypto.SecretKey;
+
+import com.android.internal.util.superior.PixelPropsUtils;
 
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
@@ -190,7 +191,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        PropImitationHooks.onEngineGetCertificateChain();
+        PixelPropsUtils.onEngineGetCertificateChain();
 
         KeyEntryResponse response = getKeyMetadata(alias);
 

random-exp/base

core/java/android/app/ApplicationPackageManager.java
@@ -137,7 +137,6 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -811,30 +810,20 @@ public class ApplicationPackageManager extends PackageManager {
                 }
             };
 
-    private static final Set<String> googlePackages = new HashSet<>(Arrays.asList(
-            "com.android.vending",
-            "com.google.android.gms",
-            "com.google.android.googlequicksearchbox",
-            "com.google.android.inputmethod.latin",
-            "com.google.android.setupwizard",
-            "com.google.android.settings.intelligence",
-            "com.google.android.apps.miphone.aiai.AiaiApplication",
-            "com.google.android.apps.googleassistant",
-            "com.google.android.as.oss",
-            "com.google.android.as",
-            "com.google.android.apps.recorder"
-    ));
+    private static final String[] pTensorCodenames = {
+            "husky",
+            "shiba",
+            "felix",
+            "tangorpro",
+            "lynx",
+            "cheetah",
+            "panther",
+            "bluejay",
+            "oriole",
+            "raven"
+    };
 
-    private static final Set<String> featuresPixelTensor = new HashSet<>(Arrays.asList(
-            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE"
-    ));
-
-    private static final Set<String> featuresPixel = new HashSet<>(Arrays.asList(
+    private static final String[] featuresPixel = {
             "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
             "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
             "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
@@ -845,6 +834,14 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.feature.LILY_EXPERIENCE",
             "com.google.android.feature.TURBO_PRELOAD",
             "com.google.android.feature.WELLBEING",
+            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2020_EXPERIENCE",
             "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
             "com.google.android.feature.PIXEL_2019_EXPERIENCE",
@@ -864,37 +861,40 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.feature.EXCHANGE_6_2",
             "com.google.android.apps.dialer.call_recording_audio",
             "com.google.android.apps.dialer.SUPPORTED"
-    ));
+    };
 
-    private static final Set<String> featuresNexus = new HashSet<>(Arrays.asList(
+    private static final String[] featuresTensor = {
+            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE"
+    };
+
+    private static final String[] featuresNexus = {
             "com.google.android.apps.photos.NEXUS_PRELOAD",
             "com.google.android.apps.photos.nexus_preload"
-    ));
+    };
     
-    private static final Set<String> featuresAndroid = new HashSet<>(Arrays.asList(
+    private static final String[] featuresAndroid = {
             "android.software.freeform_window_management"
-    ));
+    };
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
-        String packageName = ActivityThread.currentPackageName();
-        if ("com.google.android.apps.photos".equals(packageName)) {
-            if (featuresPixel.contains(name) || featuresPixelTensor.contains(name)) {
-                return false;
-            }
-            if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true) && featuresNexus.contains(name)) {
-                return true;
-            }
-        } else if (googlePackages.contains(packageName)) {
-            if (featuresPixelTensor.contains(name)) {
-                return false;
-            }
-            if (featuresPixel.contains(name)) {
-                return true;
-            }
-        } else if (featuresPixelTensor.contains(name) || featuresPixel.contains(name) || featuresAndroid.contains(name)) {
-            return true;
+        if (name != null && Arrays.asList(featuresTensor).contains(name) &&
+                !Arrays.asList(pTensorCodenames).contains(SystemProperties.get("ro.everest.device"))) {
+            return false;
         }
+        String packageName = ActivityThread.currentPackageName();
+        if (packageName != null &&
+                packageName.equals("com.google.android.apps.photos")) {
+            if (Arrays.asList(featuresPixel).contains(name)) return false;
+            if (SystemProperties.getBoolean("persist.sys.pixelprops.gphotos", true)) {
+                if (Arrays.asList(featuresNexus).contains(name)) return true;
+            }
+        }
+        if (Arrays.asList(featuresPixel).contains(name)) return true;
+        if (Arrays.asList(featuresAndroid).contains(name)) return true;
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
     }
 

core/java/android/app/Instrumentation.java
@@ -59,7 +59,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.everest.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -69,6 +68,8 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.everest.MeizuPropsUtils;
+import com.android.internal.util.everest.PixelPropsUtils;
+import com.android.internal.util.everest.GamesPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1285,10 +1286,11 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(app);
         GamesPropsUtils.setProps(app);
         return app;
     }
-    
+
     /**
      * Perform instantiation of the process's {@link Application} object.  The
      * default implementation provides the normal system behavior.
@@ -1304,6 +1306,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(app);
         GamesPropsUtils.setProps(app);
         return app;
     }

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -88,6 +88,8 @@ import java.util.NoSuchElementException;
 
 import javax.crypto.SecretKey;
 
+import com.android.internal.util.everest.PixelPropsUtils;
+
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
  * it can be created via the {@link java.security.KeyStore#getInstance(String)
@@ -189,6 +191,8 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        PixelPropsUtils.onEngineGetCertificateChain();
+
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {