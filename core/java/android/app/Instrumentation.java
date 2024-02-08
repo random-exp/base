SuperiorOS/android_frameworks_base

base: Introduce GamesPropsUtils[1/2]
Rewritten from scratch, with settings toggle
Based on the following commits:

commit e2ebc7e0d3e74bff76f1cdc56e958c1f07fd4c0b
Author: rdkng1 <richardraya39@gmail.com>
Date:   Sat Dec 11 05:17:22 2021 +0000

    PixelPropsUtils: Add GamesProps

      Based on PixelPropsUtils, GamesProps will spoof the device needed to unlock the FPS of the following games:
      * Free Fire - Spoof Asus ROG Phone 1 will unlock 90 FPS
      * COD Mobile - Spoof Xperia 5 || will unlock 120 FPS (only on multiplayer mode)
      * PUBG Mobile - Spoof OnePlus 8 Pro will unlock 90 FPS
      * Wild Rift - Spoof OnePlus 8 Pro will unlock 120 FPS
      * Cyber Hunter - Spoof OnePlus 8 Pro will unlock 90 FPS
      * Fortnite - Spoof OnePlus 8 Pro will unlock 90 FPS

    @ neobuddy89: Adapt GamesProps to existing PixelProps and update games list.

commit ce68c443c392c9be6fd0591958db960a1f10f9e3
Author: Pranav Vashi <neobuddy89@gmail.com>
Date:   Sat Mar 26 12:32:23 2022 +0530

    PixelPropsUtils: Add game props for mobile legends

commit e819f8fb14924c32abce93cccc14fd9ea69b032b
Author: Joey Huab <joey@evolution-x.org>
Date:   Mon Aug 15 18:23:48 2022 +0000

    PixelPropsUtils: Update game props

    * Removed new state as multiple crash reports.

commit 328b97baa8039e4b127585ac467846faf5f13cb9
Author: johnmart19 <johnivan19999@gmail.com>
Date:   Mon Jul 11 01:37:47 2022 +0300

    PixelPropsUtils: GameProps: Hide Apex Legends as Mi11

    Change-Id: I80b0136ef75f61154011ed7831994ffe8b5f5c96

Co-authored-by: rdkng1 <richardraya39@gmail.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Co-authored-by: johnmart19 <johnivan19999@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

core/java/android/app/Instrumentation.java
@@ -56,6 +56,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.superior.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1246,6 +1247,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(context);
         return app;
     }
     
@@ -1265,6 +1267,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(context);
         return app;
     }
 

random-exp/base

Co-authored-by: Adithya <gh0strider.2k18.reborn@gmail.com>
Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
Co-authored-by: SwapnilVicky <swapnil.motoe@gmail.com>

core/java/android/app/Instrumentation.java
@@ -59,6 +59,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.util.everest.GamesPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1284,6 +1285,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         MeizuPropsUtils.setProps(app);
+        GamesPropsUtils.setProps(app);
         return app;
     }
     
@@ -1302,6 +1304,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         MeizuPropsUtils.setProps(app);
+        GamesPropsUtils.setProps(app);
         return app;
     }
 